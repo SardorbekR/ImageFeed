c24213144c24695a11fca4b52e09df89fbf15c59
Sardor <rakhimov.sardorbek1@gmail.com>
2026-01-01 22:15:36 +0500

    initial commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Sardor on 12/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
